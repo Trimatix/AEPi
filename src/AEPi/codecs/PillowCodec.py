3ad182eda937a59ba0c8b1b71238bfc14883884a
Trimatix <1JasperLaw@gmail.com>
2024-02-24 09:04:23 +0100

    add test_PillowCodec

--- src/AEPi/codecs/PillowCodec.py
+++ src/AEPi/codecs/PillowCodec.py
@@ -3,13 +3,14 @@ from io import BytesIO
 from PIL import Image
 from ..codec import ImageCodecAdaptor, supportsFormats
 from ..constants import CompressionFormat, CompressionQuality
+from PIL.DdsImagePlugin import DdsRgbDecoder
 
+FORMAT_MODE_MAP = {
+    CompressionFormat.DXT1: "RGB",
+    CompressionFormat.DXT5: "RGBA"
+}
 
-@supportsFormats(decompresses=[
-    CompressionFormat.DXT1,
-    CompressionFormat.DXT3,
-    CompressionFormat.DXT5
-])
+@supportsFormats(decompresses=FORMAT_MODE_MAP.keys())
 class PillowCodec(ImageCodecAdaptor):
     @classmethod
     def compress(cls, im: Image.Image, format: CompressionFormat, quality: Optional[CompressionQuality]) -> bytes:
@@ -21,6 +22,9 @@ class PillowCodec(ImageCodecAdaptor):
         if format not in [CompressionFormat.DXT1, CompressionFormat.DXT3, CompressionFormat.DXT5]:
             raise ValueError(f"Codec {PillowCodec.__name__} does not support format {format.name}")
 
-        return Image.open(fp, formats=["dxt1", "dxt5"])
+        if format == CompressionFormat.DXT1:
+            mode = "RGB"
+        decoder = DdsRgbDecoder()
+        return Image.open(fp, formats=["dds"])
         
     
