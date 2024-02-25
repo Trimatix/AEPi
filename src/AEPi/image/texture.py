744ccf2f4dd33b04efa65c6e5eeb63c3262c12db
Trimatix <1JasperLaw@gmail.com>
2024-02-25 20:06:27 +0100

    add Texture.shape and size properties

--- src/AEPi/image/texture.py
+++ src/AEPi/image/texture.py
@@ -1,6 +1,29 @@
+from typing import Tuple
+
+
 class Texture:
     def __init__(self, x: int, y: int, width: int, height: int) -> None:
         self.x = x
         self.y = y
         self.width = width
         self.height = height
+
+    
+    @property
+    def size(self) -> Tuple[int, int]:
+        """The shape of the texture region, given as a (width, height) tuple.
+
+        :return: The shape of the texture region, as a (width, height) tuple
+        :rtype: Tuple[int, int]
+        """
+        return (self.width, self.height)
+
+    
+    @property
+    def position(self) -> Tuple[int, int]:
+        """The coordinates of the texture region relative to the AEI origin, given as an (x, y) tuple.
+
+        :return: The coordinates of the texture region, as an (x, y) tuple
+        :rtype: Tuple[int, int]
+        """
+        return (self.x, self.y)
