47307df2e3beb253079f8f822f1b419526d6ce98
Trimatix <1jasperlaw@gmail.com>
2024-03-18 23:46:03 +0100

    Hide unnecessary method

--- src/AEPi/__init__.py
+++ src/AEPi/__init__.py
@@ -6,4 +6,4 @@ from . import lib
 from .lib.imageOps import switchRGBA_BGRA
 
 __version__ = "0.8.1"
-__all__ = ["AEI", "Texture", "CompressionFormat", "CompressionQuality", "codecs", "lib", "switchRGBA_BGRA", "codec"]
+__all__ = ["AEI", "Texture", "CompressionFormat", "CompressionQuality", "codecs", "lib", "codec"]
