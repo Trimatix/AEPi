8e33d0813dafb456b98701dc6856ffe6bff3c723
Trimatix <1jasperlaw@gmail.com>
2024-03-18 23:45:45 +0100

    bumb version

--- src/AEPi/__init__.py
+++ src/AEPi/__init__.py
@@ -5,5 +5,5 @@ from . import codecs
 from . import lib
 from .lib.imageOps import switchRGBA_BGRA
 
-__version__ = "0.8.0.1"
+__version__ = "0.8.1"
 __all__ = ["AEI", "Texture", "CompressionFormat", "CompressionQuality", "codecs", "lib", "switchRGBA_BGRA", "codec"]
