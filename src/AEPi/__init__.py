93178e5dcce69de2a10a33459b83691bf7e5a4c1
Trimatix <1jasperlaw@gmail.com>
2024-03-23 23:08:53 +0100

    update readme, bump version

--- src/AEPi/__init__.py
+++ src/AEPi/__init__.py
@@ -4,5 +4,5 @@ from .codec import *
 from . import codecs
 from . import lib
 
-__version__ = "0.8.1"
+__version__ = "0.8.2"
 __all__ = ["AEI", "Texture", "CompressionFormat", "CompressionQuality", "codecs", "lib", "codec"]
